c07dea2ea4f358c63d9731174d0e2edb33906d3a
clonker <clonker@gmail.com>
2017-11-27 21:10:36 +0100

    relocate extension

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ class CMakeBuild(build_ext):
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        extdir = os.path.join(extdir, "pynumtools")
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable]
 
