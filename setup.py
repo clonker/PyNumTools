12808a9b267bacdd8558850a8d7bb7627a137946
Moritz Hoffmann <clonker@gmail.com>
2022-06-08 02:48:03 +0200

    test

--- setup.py
+++ setup.py
@@ -1,74 +1,37 @@
-import os
-import re
 import sys
-import platform
-import subprocess
 
-from setuptools import setup, Extension, find_packages
-from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
+from setuptools import find_packages
 
-__version__ = '0.0.1'
+try:
+    from skbuild import setup
+except ImportError:
+    print(
+        "Please update pip, you need pip 10 or greater,\n"
+        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
+        file=sys.stderr,
+    )
+    raise
 
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
+cmake_args = []
 
+metadata = \
+    dict(
+        name="pynumtools",
+        version="0.2",
+        author='Moritz Hoffmann, Christoph Fröhner',
+        author_email='clonker at gmail.com, christoph.froehner at fu-berlin.de',
+        url='https://github.com/clonker/PyNumTools',
+        description='estimator skeleton',
+        long_description='yes',
+        zip_safe=False,
+        packages=find_packages(where="."),
+        package_dir={'pynumtools': 'pynumtools'},
+        cmake_install_dir="pynumtools/",
+        install_requires=["scipy", "numpy", "tqdm"],
+        cmake_args=cmake_args,
+        include_package_data=True,
+        ext_modules=[]
+    )
 
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        extdir = os.path.join(extdir, "pynumtools")
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
-
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
-
-setup(
-    name='PyNumTools',
-    version=__version__,
-    author='Moritz Hoffmann, Christoph Fröhner',
-    author_email='clonker at gmail.com, christoph.froehner at fu-berlin.de',
-    url='https://github.com/clonker/PyNumTools',
-    description='estimator skeleton',
-    long_description='yes',
-    ext_modules=[CMakeExtension('pynumtools_binding')],
-    packages=find_packages(),
-    zip_safe=False,
-    cmdclass=dict(build_ext=CMakeBuild),
-    install_requires=['numpy', 'pybind11', 'cmake', 'eigen']
-)
+if __name__ == '__main__':
+    setup(**metadata)
